2d994b836328c795f124dccb92fb5de56b692073
Lautaro <garciarochalautaro@gmail.com>
2025-09-13 04:52:27 -0300

    Comienzo Tarea 1 de Pascal

---
+++ tarea1.pas
@@ -0,0 +1,17 @@
+{ Validación de puntos obtenidos en actividad }
+function PuntajeValidoParcial(actividad: char; puntajeObtenido: real): boolean;
+begin
+
+end;
+
+{Calculo total de puntos }
+function CalcularTotalPuntos(modalidad : char; parcial1, parcial2: real; cuestionarios: integer): real;
+begin
+
+end;
+
+{ Puntos obtenidos en cuestionarios }
+procedure LeerPuntosCuestionarios(var puntosCuestionarios: integer; var error: char);
+begin
+
+end;
